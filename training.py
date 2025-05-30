6aa36e9eb53adadb21820500e64063b1eb0f2192
Maksim-Mak <maksimskyproraven@gmail.com>
2025-05-30 22:15:59 +0300

    lesson__1_task_2

---
+++ training.py
@@ -0,0 +1,37 @@
+#my_heigh = "180"
+#print (my_heigh)
+
+#my_name = "Maxim"
+#my_name= "Maxim_Vorontsov"
+#print(my_name)
+
+#pet_name = input("Belka")
+#print("My favorite - " + pet_name)
+
+#def print_python():
+    #print("I'm learning Python!")
+
+#print_python()
+
+#def print_letter(let):
+    #print(let, end='')
+
+
+#print_letter('S')
+#print_letter('k')
+#print_letter('y')
+#print_letter('P')
+#print_letter('r')
+#print_letter('o')
+
+#def print_letter(let):
+    #print(let, end='')
+
+
+#print_letter('S')
+#print_letter('t')
+#print_letter('u')
+#print_letter('d')
+#print_letter('e')
+#print_letter('n')
+#print_letter('t')
