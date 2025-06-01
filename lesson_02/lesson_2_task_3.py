2d8989ad5ddb81b798f456ee714d9559bfc0d646
MaksimVorontsov <maximskyproraven@gmail.com>
2025-06-01 22:51:03 +0300

    Add files via upload

---
+++ lesson_02/lesson_2_task_3.py
@@ -0,0 +1,13 @@
+import math
+
+
+def square(side):
+    area = side * side
+
+    return math.ceil(area)
+
+
+side_length = 3.4
+result = square(side_length)
+
+print(f"Площадь квадрата со стороной {side_length} равна {result}.")
